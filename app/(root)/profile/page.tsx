8a7d9d677e13a292ffb4817993ea0ef8f66706f2
Ser25 <shatsovniy25@gmail.com>
2025-03-10 14:54:53 +0200

    feat: refactor authentication logic and improve session handling with user redirection

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,3 +1,11 @@
-export default function ProfilePage() {
+import { getUserSession } from '@/shared/lib/get-user-session';
+import { redirect } from 'next/navigation';
+
+export default async function ProfilePage() {
+  const session = await getUserSession();
+
+  if (!session) {
+    return redirect('/not-auth');
+  }
   return <div>Це твій профіль</div>;
 }
--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './checkout-form-schema';
 export * from './pizza';
+export * from './auth-options';
