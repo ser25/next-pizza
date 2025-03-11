e2b2c9ed9f9208c976ac09773cbbab6c0ef71304
Ser25 <shatsovniy25@gmail.com>
2025-03-11 13:09:42 +0200

    feat: add Google authentication provider to auth options

--- shared/constants/auth-options.ts
+++ shared/constants/auth-options.ts
@@ -4,9 +4,14 @@ import { compare, hashSync } from 'bcryptjs';
 import { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: AuthOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
